9cb424725cf629b5e18961505d39546bdf9945b5
bgwnada <bigwinent1@outlook.com>
2026-02-12 16:47:00 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
  
 export default function Page() {
-  return <Image src="/assets.png" alt="Profile" width={100} height={100} />
+  return <Image src="/assets/bgwn_records_logo.png" alt="Profile" width={100} height={100} />
 }
